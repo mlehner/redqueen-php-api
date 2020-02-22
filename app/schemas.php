8041218122b35384355b220fe95e1c6c37362445
Matt Lehner <mlehner@gmail.com>
2020-02-22 16:40:17 -0500

    fix column types for Doctrine DBAL

--- app/schemas.php
+++ app/schemas.php
@@ -6,29 +6,29 @@ $primarySchema = new Schema();
 
 $cardTable = $primarySchema->createTable('cards');
 
-$cardTable->addColumn('id', 'bigint', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'length' => 20]);
+$cardTable->addColumn('id', 'integer', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'length' => 20]);
 $cardTable->setPrimaryKey(['id']);
 
-$cardTable->addColumn('name', 'varchar', ['length' => 255, 'notnull' => true]);
-$cardTable->addColumn('code', 'varchar', ['length' => 6, 'notnull' => true]);
-$cardTable->addColumn('pin', 'varchar', ['length' => 32, 'notnull' => true]);
-$cardTable->addColumn('isActive', 'tinyint', ['length' => 1, 'default' => '1', 'notnull' => true]);
+$cardTable->addColumn('name', 'string', ['length' => 255, 'notnull' => true]);
+$cardTable->addColumn('code', 'string', ['length' => 6, 'notnull' => true]);
+$cardTable->addColumn('pin', 'string', ['length' => 32, 'notnull' => true]);
+$cardTable->addColumn('isActive', 'boolean', ['default' => true, 'notnull' => true]);
 $cardTable->addColumn('created_at', 'datetime', ['notnull' => true]);
 $cardTable->addColumn('updated_at', 'datetime', ['notnull' => true]);
 
 $scheduleTable = $primarySchema->createTable('schedules');
 
-$scheduleTable->addColumn('id', 'bigint', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'length' => 20]);
+$scheduleTable->addColumn('id', 'integer', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'length' => 20]);
 $scheduleTable->setPrimaryKey(['id']);
 
-$scheduleTable->addColumn('name', 'varchar', ['length' => 255, 'notnull' => true]);
-$scheduleTable->addColumn('mon', 'tinyint', ['length' => 1, 'notnull' => true, 'default' => '0']);
-$scheduleTable->addColumn('tue', 'tinyint', ['length' => 1, 'notnull' => true, 'default' => '0']);
-$scheduleTable->addColumn('wed', 'tinyint', ['length' => 1, 'notnull' => true, 'default' => '0']);
-$scheduleTable->addColumn('thu', 'tinyint', ['length' => 1, 'notnull' => true, 'default' => '0']);
-$scheduleTable->addColumn('fri', 'tinyint', ['length' => 1, 'notnull' => true, 'default' => '0']);
-$scheduleTable->addColumn('sat', 'tinyint', ['length' => 1, 'notnull' => true, 'default' => '0']);
-$scheduleTable->addColumn('sun', 'tinyint', ['length' => 1, 'notnull' => true, 'default' => '0']);
+$scheduleTable->addColumn('name', 'string', ['length' => 255, 'notnull' => true]);
+$scheduleTable->addColumn('mon', 'boolean', ['notnull' => true, 'default' => false]);
+$scheduleTable->addColumn('tue', 'boolean', ['notnull' => true, 'default' => false]);
+$scheduleTable->addColumn('wed', 'boolean', ['notnull' => true, 'default' => false]);
+$scheduleTable->addColumn('thu', 'boolean', ['notnull' => true, 'default' => false]);
+$scheduleTable->addColumn('fri', 'boolean', ['notnull' => true, 'default' => false]);
+$scheduleTable->addColumn('sat', 'boolean', ['notnull' => true, 'default' => false]);
+$scheduleTable->addColumn('sun', 'boolean', ['notnull' => true, 'default' => false]);
 $scheduleTable->addColumn('startTime', 'time', ['notnull' => true]);
 $scheduleTable->addColumn('endTime', 'time', ['notnull' => true]);
 $scheduleTable->addColumn('created_at', 'datetime', ['notnull' => true]);
@@ -36,18 +36,18 @@ $scheduleTable->addColumn('updated_at', 'datetime', ['notnull' => true]);
 
 $cardScheduleTable = $primarySchema->createTable('card_schedule');
 
-$cardScheduleTable->addColumn('card_id', 'bigint', ['length' => 20, 'unsigned' => true, 'notnull' => true]);
-$cardScheduleTable->addColumn('schedule_id', 'bigint', ['length' => 20, 'unsigned' => true, 'notnull' => true]);
+$cardScheduleTable->addColumn('card_id', 'integer', ['length' => 20, 'unsigned' => true, 'notnull' => true]);
+$cardScheduleTable->addColumn('schedule_id', 'integer', ['length' => 20, 'unsigned' => true, 'notnull' => true]);
 
 $logSchema = new Schema();
 
 $logTable = $logSchema->createTable('logs');
 
-$logTable->addColumn('id', 'bigint', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'length' => 20]);
+$logTable->addColumn('id', 'integer', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'length' => 20]);
 $logTable->setPrimaryKey(['id']);
 
-$logTable->addColumn('code', 'varchar', ['length' => 6, 'notnull' => true]);
-$logTable->addColumn('validPin', 'tinyint', ['length' => 1, 'notnull' => true, 'default' => '0']);
+$logTable->addColumn('code', 'string', ['length' => 6, 'notnull' => true]);
+$logTable->addColumn('validPin', 'boolean', ['notnull' => true, 'default' => false]);
 $logTable->addColumn('created_at', 'datetime', ['notnull' => true]);
 
 return [
