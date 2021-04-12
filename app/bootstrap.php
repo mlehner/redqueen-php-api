c7b513134493a4050c669f98aa57e14a4a1c28a5
Matt Lehner <matt.lehner@coachmeplus.com>
2021-04-11 20:51:59 -0400

    grab debug config from environment

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,6 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
-$app['debug'] = true;
+$app['debug'] = isset($_SERVER['APP_DEBUG']) ? (bool) $_SERVER['APP_DEBUG'] : isset($_ENV['APP_DEBUG']) ? (bool) $_ENV['APP_DEBUG'] : false;
 
 require_once __DIR__ . '/providers.php';
