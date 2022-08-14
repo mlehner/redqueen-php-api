7ecdf983ec83905951ea5358da8e5f369e5c4855
Matt Lehner <matt.lehner@coachmeplus.com>
2022-08-14 14:51:27 -0400

    fix debug flag

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
-$app['debug'] = isset($_SERVER['APP_DEBUG']) ? (bool) $_SERVER['APP_DEBUG'] : isset($_ENV['APP_DEBUG']) ? (bool) $_ENV['APP_DEBUG'] : false;
+$app['debug'] = isset($_SERVER['APP_DEBUG']) ? (bool) $_SERVER['APP_DEBUG'] : (isset($_ENV['APP_DEBUG']) ? (bool) $_ENV['APP_DEBUG'] : false);
 
 require_once __DIR__ . '/providers.php';
