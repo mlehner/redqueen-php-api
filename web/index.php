2d7bd9f41f8228a5cf4e0cd085d4d150af31cd7c
Matt Lehner <matt.lehner@coachmeplus.com>
2022-08-14 14:51:48 -0400

    do not show errors in responses

--- web/index.php
+++ web/index.php
@@ -3,7 +3,8 @@
 declare(strict_types=1);
 
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 'off');
+ini_set('log_errors', 'on');
 
 require_once __DIR__ . '/../app/bootstrap.php';
 require_once __DIR__ . '/../app/routes.php';
