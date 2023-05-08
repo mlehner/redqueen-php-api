a5161afa79733ec3a141dffa8a34e9fa43a32fc8
Matt Lehner <matt.lehner@coachmeplus.com>
2023-05-07 23:52:51 -0400

    add some indexes

--- app/schemas.php
+++ app/schemas.php
@@ -9,20 +9,21 @@ $primarySchema = new Schema();
 $cardTable = $primarySchema->createTable('cards');
 
 $cardTable->addColumn('id', 'bigint', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'length' => 20]);
-$cardTable->setPrimaryKey(['id']);
-
 $cardTable->addColumn('name', 'string', ['length' => 255, 'notnull' => true]);
+// @TODO this should be unique
 $cardTable->addColumn('code', 'string', ['length' => 6, 'notnull' => true]);
 $cardTable->addColumn('pin', 'string', ['length' => 32, 'notnull' => true]);
 $cardTable->addColumn('isActive', 'boolean', ['default' => true, 'notnull' => true]);
 $cardTable->addColumn('created_at', 'datetime', ['notnull' => true]);
 $cardTable->addColumn('updated_at', 'datetime', ['notnull' => true]);
 
+$cardTable->setPrimaryKey(['id']);
+$cardTable->addIndex(['code'], 'code');
+$cardTable->addIndex(['isActive'], 'isActive');
+
 $scheduleTable = $primarySchema->createTable('schedules');
 
 $scheduleTable->addColumn('id', 'bigint', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'length' => 20]);
-$scheduleTable->setPrimaryKey(['id']);
-
 $scheduleTable->addColumn('name', 'string', ['length' => 255, 'notnull' => true]);
 $scheduleTable->addColumn('mon', 'boolean', ['unsigned' => true, 'notnull' => true, 'default' => false]);
 $scheduleTable->addColumn('tue', 'boolean', ['unsigned' => true, 'notnull' => true, 'default' => false]);
@@ -36,21 +37,27 @@ $scheduleTable->addColumn('endTime', 'time', ['notnull' => true]);
 $scheduleTable->addColumn('created_at', 'datetime', ['notnull' => true]);
 $scheduleTable->addColumn('updated_at', 'datetime', ['notnull' => true]);
 
-$cardScheduleTable = $primarySchema->createTable('card_schedule');
+$scheduleTable->setPrimaryKey(['id']);
 
+$cardScheduleTable = $primarySchema->createTable('card_schedule');
 $cardScheduleTable->addColumn('card_id', 'bigint', ['length' => 20, 'unsigned' => true, 'notnull' => true]);
 $cardScheduleTable->addColumn('schedule_id', 'bigint', ['length' => 20, 'unsigned' => true, 'notnull' => true]);
+
+$cardScheduleTable->setPrimaryKey(['card_id', 'schedule_id']);
 
 $logSchema = new Schema();
 
 $logTable = $logSchema->createTable('logs');
 
 $logTable->addColumn('id', 'bigint', ['unsigned' => true, 'notnull' => true, 'autoincrement' => true, 'length' => 20]);
-$logTable->setPrimaryKey(['id']);
-
 $logTable->addColumn('code', 'string', ['length' => 6, 'notnull' => true]);
 $logTable->addColumn('validPin', 'boolean', ['notnull' => true, 'default' => false]);
 $logTable->addColumn('created_at', 'datetime', ['notnull' => true]);
+
+$logTable->setPrimaryKey(['id']);
+$logTable->addIndex(['code'], 'code');
+$logTable->addIndex(['validPin'], 'validPin');
+$logTable->addIndex(['created_at'], 'created_at');
 
 return [
   'primary' => $primarySchema,
