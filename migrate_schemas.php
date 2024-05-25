07460d3358a5d8171a90abf2e9b25410cb744a3b
Matt Lehner <mlehner@gmail.com>
2024-05-25 10:04:48 -0300

    use Schema utilities in migrate_schemas.php

--- migrate_schemas.php
+++ migrate_schemas.php
@@ -2,49 +2,46 @@
 
 declare(strict_types=1);
 
+use BLInc\Migration\SchemaLoader;
+use BLInc\Migration\SchemaTool;
 use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Schema\Comparator;
 use Doctrine\DBAL\Schema\Schema;
 
 require_once __DIR__ . '/app/bootstrap.php';
 
 $applyMigrations = ($argc === 2 && $argv[1] === '--force');
 
-/** @var Schema[] $schemas */
-$schemas = require __DIR__ . '/app/schemas.php';
-
 $migrateSchema = function (Schema $schema, Connection $connection, $applyMigrations) {
-  $schemaManager = $connection->getSchemaManager();
+  $queries = (new SchemaTool($connection))->generateSql($schema);
 
-  $fromSchema = $schemaManager->createSchema();
-
-  $comparator = new Comparator();
-  $schemaDiff = $comparator->compare($fromSchema, $schema);
-
-  $queries = $schemaDiff->toSaveSql($connection->getDatabasePlatform());
-
-  if (count($queries) === 0) {
-    echo "No Migration Needed!\n";
-    return;
-  }
-
+  $queryCount = 0;
   echo "Queries To Run\n\n";
+
   foreach ($queries as $query) {
+    $queryCount++;
     echo $query."\n";
+
     if ($applyMigrations) {
       $connection->exec($query);
     }
   }
 
+  if ($queryCount === 0) {
+    echo "No Migration Needed!\n";
+    return;
+  }
+
   echo "\n";
 };
+
+$schemaLoader = new SchemaLoader();
 
 /** @var Connection $primaryConnection */
 $primaryConnection = $app['db'];
 
-$migrateSchema($schemas['primary'], $primaryConnection, $applyMigrations);
+$migrateSchema($schemaLoader->getPrimarySchema(), $primaryConnection, $applyMigrations);
 
 /** @var Connection $logConnection */
 $logConnection = $app['dbs']['log'];
 
-$migrateSchema($schemas['log'], $logConnection, $applyMigrations);
+$migrateSchema($schemaLoader->getLogSchema(), $logConnection, $applyMigrations);
