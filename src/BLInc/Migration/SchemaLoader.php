19563cea8c38629d3930b97ee71310fb0e8d3842
Matt Lehner <mlehner@gmail.com>
2024-05-25 10:02:01 -0300

    add Schema utilities

---
+++ src/BLInc/Migration/SchemaLoader.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BLInc\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+
+final class SchemaLoader
+{
+  private array $schemas;
+
+  public function getPrimarySchema(): Schema
+  {
+    $this->loadSchemas();
+    return $this->schemas['primary'];
+  }
+
+  public function getLogSchema(): Schema
+  {
+    $this->loadSchemas();
+    return $this->schemas['log'];
+  }
+
+  private function loadSchemas()
+  {
+    if (isset($this->schemas['primary'])) {
+      return;
+    }
+
+    $this->schemas = require __DIR__ . '/../../../app/schemas.php';
+  }
+}
---
+++ src/BLInc/Migration/SchemaTool.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BLInc\Migration;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Schema\Comparator;
+use Doctrine\DBAL\Schema\Schema;
+
+final class SchemaTool
+{
+  private Connection $connection;
+
+  public function __construct(Connection $connection)
+  {
+    $this->connection = $connection;
+  }
+
+  public function recreateDatabase(): void
+  {
+    $database = $this->connection->getDatabase();
+    $this->connection->getSchemaManager()->tryMethod('dropDatabase', $database);
+    $this->connection->getSchemaManager()->createDatabase($database);
+    $this->connection->close();
+    $this->connection->connect();
+  }
+
+  public function generateSql(Schema $schema): iterable
+  {
+      $schemaManager = $this->connection->getSchemaManager();
+
+      $fromSchema = $schemaManager->createSchema();
+
+      $comparator = new Comparator();
+      $schemaDiff = $comparator->compare($fromSchema, $schema);
+
+      return $schemaDiff->toSaveSql($this->connection->getDatabasePlatform());
+  }
+}
