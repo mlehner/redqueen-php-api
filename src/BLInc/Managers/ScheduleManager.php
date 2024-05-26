b0ea7d4897983d8f7862b0a6dc7aa2cc6b2405d0
Matt Lehner <mlehner@gmail.com>
2024-05-26 14:28:24 -0300

    remove unused findByCard

--- src/BLInc/Managers/ScheduleManager.php
+++ src/BLInc/Managers/ScheduleManager.php
@@ -8,14 +8,7 @@ use Doctrine\DBAL\Connection;
 
 class ScheduleManager extends TimestampedManager
 {
-
-  public function findByCard($cardId)
-  {
-    $query = 'SELECT s.*,cs.card_id FROM schedules s LEFT JOIN card_schedule cs ON (s.id = cs.schedule_id) WHERE cs.card_id = :cardId';
-    return array_map([$this, 'transformRow'], $this->dbal->fetchAll($query, array('cardId' => $cardId)));
-  }
-
-  public function findByCards(array $cardIds)
+  public function findByCards(array $cardIds): iterable
   {
     $query = 'SELECT s.*,cs.card_id FROM schedules s LEFT JOIN card_schedule cs ON (s.id = cs.schedule_id) WHERE cs.card_id IN (:cardIds)';
     return array_map([$this, 'transformRow'], $this->dbal->fetchAll($query, array('cardIds' => $cardIds), ['cardIds' => Connection::PARAM_INT_ARRAY]));
