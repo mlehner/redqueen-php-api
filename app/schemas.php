7873931e104235012781dbcc410ab987f01bcbc3
Matt Lehner <mlehner@gmail.com>
2024-09-24 12:50:23 -0400

    add foreign keys for cards, schedules and doors

--- app/schemas.php
+++ app/schemas.php
@@ -46,6 +46,8 @@ $cardScheduleTable->addColumn('schedule_id', 'bigint', ['length' => 20, 'unsigne
 $cardScheduleTable->addColumn('created_at', 'datetime', ['notnull' => true, 'default' => 'CURRENT_TIMESTAMP']);
 
 $cardScheduleTable->setPrimaryKey(['card_id', 'schedule_id']);
+$cardScheduleTable->addForeignKeyConstraint('cards', ['card_id'], ['id'], ['onDelete' => 'CASCADE']);
+$cardScheduleTable->addForeignKeyConstraint('schedules', ['schedule_id'], ['id'], ['onDelete' => 'RESTRICT']);
 
 $doorTable = $primarySchema->createTable('doors');
 
@@ -64,6 +66,8 @@ $doorScheduleTable->addColumn('schedule_id', 'bigint', ['length' => 20, 'unsigne
 $doorScheduleTable->addColumn('created_at', 'datetime', ['notnull' => true, 'default' => 'CURRENT_TIMESTAMP']);
 
 $doorScheduleTable->setPrimaryKey(['door_id', 'schedule_id']);
+$doorScheduleTable->addForeignKeyConstraint('doors', ['door_id'], ['id'], ['onDelete' => 'CASCADE']);
+$doorScheduleTable->addForeignKeyConstraint('schedules', ['schedule_id'], ['id'], ['onDelete' => 'RESTRICT']);
 
 $logSchema = new Schema();
 
