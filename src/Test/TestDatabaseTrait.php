5b913eaeebc283876615f12a0149214bc0101207
Matt Lehner <mlehner@gmail.com>
2025-11-30 11:49:15 -0500

    remove log connection from TestDatabaseTrait

--- src/Test/TestDatabaseTrait.php
+++ src/Test/TestDatabaseTrait.php
@@ -28,10 +28,8 @@ trait TestDatabaseTrait
     private static function rebuildDatabase(): void
     {
         $primarySchemaTool = self::rebuildPrimaryDatabase();
-        $logSchemaTool = self::rebuildLogDatabase();
 
         self::rebuildPrimarySchema($primarySchemaTool);
-        self::rebuildLogSchema($logSchemaTool);
     }
 
     private static function rebuildPrimaryDatabase(): SchemaTool
@@ -48,58 +46,20 @@ trait TestDatabaseTrait
         $schemaTool->executeSchemaSql($schemaLoader->getPrimarySchema());
     }
 
-    private static function rebuildLogDatabase(): SchemaTool
-    {
-        $logConnection = self::logConnection();
-        $schemaTool = new SchemaTool($logConnection);
-        $schemaTool->recreateDatabase();
-
-        return $schemaTool;
-    }
-
-    private static function rebuildLogSchema(SchemaTool $schemaTool): void
-    {
-        $schemaLoader = new SchemaLoader();
-        $schemaTool->executeSchemaSql($schemaLoader->getLogSchema());
-    }
-
     private static function beforeTest(): void
     {
         $primaryConnection = self::primaryConnection();
-        $logConnection = self::logConnection();
 
         foreach ($primaryConnection->getSchemaManager()->listTables() as $table) {
             $primaryConnection->executeStatement('ALTER TABLE `' . $table->getName() . '` AUTO_INCREMENT=1');
         }
 
-        foreach ($logConnection->getSchemaManager()->listTables() as $table) {
-            $logConnection->executeStatement('ALTER TABLE `' . $table->getName() . '` AUTO_INCREMENT=1');
-        }
 
         $primaryConnection->beginTransaction();
-        $logConnection->beginTransaction();
-    }
-
-    private static function afterTest(): void
-    {
-        $primaryConnection = self::primaryConnection();
-        $primaryConnection->rollBack();
-
-        $logConnection = self::logConnection();
-        $logConnection->rollBack();
     }
 
     private static function primaryConnection(): Connection
     {
-        global $app;
-        assert($app['db'] instanceof Connection);
-        return $app['db'];
-    }
-
-    private static function logConnection(): Connection
-    {
-        global $app;
-        assert($app['dbs']['log'] instanceof Connection);
-        return $app['dbs']['log'];
+        return self::getContainer()->get('doctrine.dbal.primary_connection');
     }
 }
